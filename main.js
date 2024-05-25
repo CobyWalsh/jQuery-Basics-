9ea078d6c3562737173a81e1c495726a9f406a91
Coby Walsh <cobywalsh7@yahoo.com>
2024-05-25 12:08:37 -0600

    created an animated scroll on page load

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 $(document).ready(function() {
 
+    // This fuction uses a scroll animation. When the button is clicked is scrolls to the desired section
 var page_url = window.location.href;
 var page_id = page_url.substring(page_url.lastIndexOf("#") + 1);
 if(page_id == "section3") {
     $("html, body").animate({
-        screenTop: $("scroll-" + page_id).offset().top
+        screenTop: $("scroll-" + page_id).offset().top - 20
     }, 1000);
 } else if (page_id == "post") {
     $("html, body").animate({
